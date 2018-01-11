7960ebf687193fd095b5e13917553b17533a6858
KiernanDurkin <32209862+KiernanDurkin@users.noreply.github.com>
2018-01-10 19:53:33 -0500

    Add files via upload
    cant figure out spotfiy

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key:'SwzesVXRdwtkDMRlqb7qokxf4',
+  consumer_secret:'DC9W8FFkjjru1HFYc8QTTkJjJGHGKkgENU6ZhD6J5nYHQGdw1y',
+  access_token_key:'950839917482999808-wnIXPQ5rlp2Boq5o0hAnQZQ9zfAJZfB',
+  access_token_secret:'z3rgElJ6dIFIjexruFoKyhwmPa6Y3Qm2qwXhDKI1uFHoI'
+}
+
+module.exports = twitterKeys;
